332d99e84ee2f79cc390adb1dce4815698e5afce
DhwanilChaudhari <dhwanil899@gmail.com>
2024-12-12 09:58:45 +0530

    add

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const path = require("path");
 const hbs = require("hbs");
-const PORT = process.env.PORT || 3000;
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = process.env.PORT;
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const viewPath = path.join(__dirname, "../templates/views");
 const partialPath = path.join(__dirname, "../templates/partials");
@@ -12,12 +24,11 @@ const publicPath = path.join(__dirname, "../public");
 app.set("view engine", "hbs");
 app.set("views", viewPath);
 app.use(express.static(publicPath));
+hbs.registerPartials(partialPath);
 
-app.get("/", (req, resp) => {
-  resp.render("index");
-});
+app.use("/", require("../router/userRouter"));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`server running on ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });
